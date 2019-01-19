d1b7e8dcdd23a9b31ddfa0e53448a4f6e470ebf7
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-01-19 16:42:49 +0900

    finish 6.2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,7 +44,21 @@ class UserTest < ActiveSupport::TestCase
                             foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
-      assert_not @user.valid_, "#{invalid_address.inspect} should be invalid"
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.COM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
